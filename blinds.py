c22e1291226e4fefbba64b2a829331de9262f9bb
Paul Hein <dehein@gmail.com>
2017-10-13 20:17:08 +0200

    Started to make everything more accessible for other pins

---
+++ blinds.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Use the pin number as argument for the script
+
+import RPi.GPIO as GPIO
+import time
+import sys  # to be possible to import argument
+
+
+### the rest still to be done but through atom not the cli anymore ###
